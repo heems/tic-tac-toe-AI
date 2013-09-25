c98bbeb3cd5136c6f0f96239cf6bca102ba4164f
Karthik Bala <drmelonhead@gmail.com>
2013-09-24 20:17:09 -0700

    finished basic game

--- game.py
+++ game.py
@@ -1,5 +1,7 @@
 board = [[" " for i in range(0, 3)] for i in range(0, 3)]
 
+print board
+
 def print_board():
 	print "\n\n"	
 	print "  1 2 3\n"
@@ -25,7 +27,7 @@ def win(player):
 	#horizontal
 	for r in range(0, 3):
 		for c in range(0, 3):
-			if[r][c] != player:
+			if board[r][c] != player:
 				break
 			if c == 2:
 				return True
@@ -33,15 +35,38 @@ def win(player):
 	#vertical
 	for r in range(0, 3):
 		for c in range(0, 3):
-			if[c][r] != player:
+			if board[c][r] != player:
 				break
 			if r == 2:
 				return True
 
 	#diagonal
+	for r in range(0, 3):
+		if board[r][r] != player:
+			break
+		if r == 2:
+			return True
+
+	#reverse diagonal
+	for r in range(2, -1, -1):
+		if board[r][r] != player:
+			break
+		if r == 0:
+			return True
+
+def computer_move():
+	print "computer"
+
 
 
 current_player = "X"
 while not win(current_player):
 	print_board()
-	choose_spot()
+	choose_spot() if current_player == "X" else computer_move()
+	if current_player == "X":
+		current_player = "O"
+	else:
+		current_player = "X"
+
+print_board()
+print current_player + " has won."
